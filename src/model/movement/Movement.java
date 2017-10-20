64c2671851d57fd7ff2eafe558e335ae536c183a
Richard Tseng <rt120@duke.edu>
2017-10-20 14:17:20 -0400

    outline

---
+++ src/model/movement/Movement.java
@@ -0,0 +1,15 @@
+package model.movement;
+
+import model.turtle.Turtle;
+
+/**
+ * @author richardtseng
+ *	
+ */
+public abstract class Movement {
+	
+	protected int move(Turtle t, int pixels) {
+		
+		return pixels;
+	}
+}
