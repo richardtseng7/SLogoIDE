f1a614137ab182d613ed54f2a27d9b35a7a74605
Richard Tseng <rt120@duke.edu>
2017-10-22 21:06:31 -0400

    Updated movement, turn

--- src/model/movement/Movement.java
+++ src/model/movement/Movement.java
@@ -4,17 +4,15 @@ import model.turtle.Turtle;
 import javafx.geometry.Point2D;
 /**
  * @author richardtseng
- *         paullee
  *	
  */
 public abstract class Movement {
 	protected double move(Turtle t, int direction, int pixels) {
-		Point2D pos = t.getPos();
-		double x = pos.getX();
-		double unitX = x/pos.magnitude();
-		double y = pos.getY();
-		double unitY = y/pos.magnitude();
-		Point2D newPos = new Point2D(x+unitX*pixels*direction, y+unitY*pixels*direction);
+		Point2D oldPos = t.getPos();
+		double angle = Math.toRadians(t.getHeading());
+		double x = oldPos.getX() + (direction * pixels * Math.sin(angle));
+		double y = oldPos.getY() + (direction * pixels * Math.cos(angle));
+		Point2D newPos = new Point2D(x, y);
 		t.setPos(newPos);
 		return pixels;
 	}
