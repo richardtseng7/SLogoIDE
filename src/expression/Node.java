abda3f3aabe68ac964225227afdc7a46c0f13eaf
Paul Lee <chia.fu.lee@duke.edu>
2017-10-28 21:33:40 -0400

    Update Node.java

--- src/expression/Node.java
+++ src/expression/Node.java
@@ -14,12 +14,37 @@ public class Node {
 		value = item;
 		left = right = null;
 	}
-	
-	protected double execute(Turtle t) {
-		return 0;
-	};
-	
 	protected int getNumParam() {
 		return 0;
 	}
+	
+	private Turtle myT;
+	private String mySymbol;
+	private Executor c1, c2;
+	
+	public double execute(Turtle t){
+		return 0;
+	}
+	
+	public void setTurtle(Turtle t){
+		myT = t;
+	}
+	
+	
+	
+	public String getItem(){
+		return value;
+	}
+	
+	public String getType(){
+		return mySymbol;
+	}
+	public void setCond1(Executor e){
+		c1 = e;
+	}
+	
+	public void setCond2(Executor e){
+		c2 = e;
+	}
+	
 }
