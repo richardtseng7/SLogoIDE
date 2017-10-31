97ba437c7b40e15dface20992f439ee15822f0ce
Nathan Lewis <nathanblaiselewis@gmail.com>
2017-10-31 04:03:41 -0400

    Added button input functionality

--- src/model/commands/Movement.java
+++ src/model/commands/Movement.java
@@ -10,18 +10,11 @@ public abstract class Movement extends Command{
 	
 	protected double move(Turtle t, double distance) {
 		Point2D oldPos = t.getPos();
-		System.out.println(distance);
-		System.out.println(t.getXCor());
-		System.out.println(t.getYCor());
 		double angle = Math.toRadians(t.getHeading());
 		double x = oldPos.getX() - (distance * Math.cos(angle));
 		double y = oldPos.getY() - (distance * Math.sin(angle));
 		Point2D newPos = new Point2D(x, y);
-		System.out.println(newPos.getX());
-		System.out.println(newPos.getY());
 		t.setPos(newPos);
-		System.out.println(t.getXCor());
-		System.out.println(t.getYCor());
 		return distance;
 	}
 }
