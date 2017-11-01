f20cdaf9b6853051e8c7ded656bbcfe075a43682
Nathan Lewis <nathanblaiselewis@gmail.com>
2017-10-31 23:20:38 -0400

    Minor change to fix turtle image selection and y display

--- src/gui/TurtleInfoTabs.java
+++ src/gui/TurtleInfoTabs.java
@@ -28,7 +28,7 @@ public class TurtleInfoTabs {
 			infoList.getItems().add("ID: "+i);
 			infoList.getItems().add("Position:");
 			infoList.getItems().add("x - " + m.getTurtle(i).getXCor());
-			infoList.getItems().add("y - " + m.getTurtle(i).getYCor());
+			infoList.getItems().add("y - " + (-m.getTurtle(i).getYCor()));
 			infoList.getItems().add("Heading: " + m.getTurtle(i).getHeading());
 			tab.setContent(infoList);
 			tabList.add(tab);
